0e58a53dc263ff1fa8e16200b8afe7eb10f638a7
Eric Wang <30314019+AnichWang@users.noreply.github.com>
2018-12-19 23:25:00 +0800

    updata fetch data function

--- com/requestTool/InterfaceTest_kaola.py
+++ com/requestTool/InterfaceTest_kaola.py
@@ -1,14 +1,19 @@
 from urllib import request, parse
+import json
 
 if __name__ == "__main__":
     req = request.Request('http://oauthuc-test.supergina.cn/user/sendCoupon')
+
+    jsondata = json.dumps({"phone":"17710324432","requestId":"nsjdl","sign":"NCLQ49zd2Nuq6vZ9R9/F/YU0J7rXqQvoQeTufhY5R1h+Be4xQM1/2THd9zGx8syABfhoES2y7jp8N6dSiIIpPOXzmveEJAsaJSPE9JFcFFw\u003d","appkey":"f47967e9e88a3644","timestamp":1545209593})
+    jsondata = bytes(jsondata, 'utf8')
+
     data = parse.urlencode([('phone', 17710324432), ('requestId', 'nsjdl'),
                             ('sign',
                              'NCLQ49zd2Nuq6vZ9R9/F/YU0J7rXqQvoQeTufhY5R1h+Be4xQM1/2THd9zGx8syABfhoES2y7jp8N6dSiIIpPOXzmveEJAsaJSPE9JFcFFw\u003d'),
                             ("appkey", 'f47967e9e88a3644'), ('timestamp', 1545209593)])
     req.add_header('Content-Type', 'application/json')
 
-    with request.urlopen(req, data.encode('utf-8')) as f:
+    with request.urlopen(req, jsondata) as f:
         result = f.read().decode('utf-8')
         print('response code:'+str(f.status)+',content'+f.reason)
         print(result)
--- com/requestTool/Test_requests_Packege.py
+++ com/requestTool/Test_requests_Packege.py
@@ -1,4 +1,8 @@
 import requests
+import json
 
-r = requests.post('http://oauthuc-test.supergina.cn/user/sendCoupon', data={"phone": "17710324432", "requestId": "nsjdl", "sign": "NCLQ49zd2Nuq6vZ9R9/F/YU0J7rXqQvoQeTufhY5R1h+Be4xQM1/2THd9zGx8syABfhoES2y7jp8N6dSiIIpPOXzmveEJAsaJSPE9JFcFFw\\u003d", "appkey": "f47967e9e88a3644", "timestamp": 1545209593} )
+data = json.dumps({"phone":"17710324432","requestId":"nsjdl","sign":"NCLQ49zd2Nuq6vZ9R9/F/YU0J7rXqQvoQeTufhY5R1h+Be4xQM1/2THd9zGx8syABfhoES2y7jp8N6dSiIIpPOXzmveEJAsaJSPE9JFcFFw\u003d","appkey":"f47967e9e88a3644","timestamp":1545209593})
+
+#r = requests.post('http://oauthuc-test.supergina.cn/user/sendCoupon', data={"phone": "17710324432", "requestId": "nsjdl", "sign": "NCLQ49zd2Nuq6vZ9R9/F/YU0J7rXqQvoQeTufhY5R1h+Be4xQM1/2THd9zGx8syABfhoES2y7jp8N6dSiIIpPOXzmveEJAsaJSPE9JFcFFw\\u003d", "appkey": "f47967e9e88a3644", "timestamp": 1545209593} )
+r = requests.post('http://oauthuc-test.supergina.cn/user/sendCoupon',data)
 print(r.json)
