a6e6dc7a33e472158e6379333c73aadc57a18470
Alto <30314019+AnichWang@users.noreply.github.com>
2019-05-24 13:43:03 +0800

    update JYC content

---
+++ com/JYC/CalculateProfit.py
@@ -0,0 +1,18 @@
+"""
+计算收益脚本
+"""
+
+
+def calculate_profit(establish_data, duration, valuedate):
+    """
+    :param establish_data: 产品成立日
+    :param duration: 产品期限
+    :param valuedate: 起息日
+    :return:
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    print('test')
